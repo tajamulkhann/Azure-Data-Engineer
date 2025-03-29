df58ca02d6ddb56fdc6cab636d34ef6a74083304
Tajamul Khan <tajamulk@gmail.com>
2025-03-30 02:40:56 +0530

    Update 08.sql

--- Azure DP 203/4. Azure Synapse Analytics/Code/8. Lab - Loading data into a SQL pool using Polybase/08.sql
+++ Azure DP 203/4. Azure Synapse Analytics/Code/8. Lab - Loading data into a SQL pool using Polybase/08.sql
@@ -1,9 +1,10 @@
+-- Loading Data From External Table into Synapse SQL Pool using POLYBASE
 
 CREATE TABLE PoolActivityLog
 WITH(
     DISTRIBUTION=ROUND_ROBIN
 )
 AS 
-SELECT * FROM ActivityLog;
+SELECT * FROM ActivityLog1;
 
 SELECT * FROM PoolActivityLog;
